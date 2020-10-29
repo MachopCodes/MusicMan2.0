e555b3394c282d65d3caa2739f546a58d1f25b4b
MachopCodes <william.andreae@uconn.edu>
2020-10-29 14:49:23 -0400

    refactored container

--- src/components/Profile/Cards/ProfileCards.js
+++ src/components/Profile/Cards/ProfileCards.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react'
 import { Link, withRouter } from 'react-router-dom'
-import { Container, Row, Col, Card, Button, Accordion } from 'react-bootstrap'
+import { Card, Button, Accordion } from 'react-bootstrap'
 import ReviewPost from '../../Review/ReviewPost'
 import ReviewPatch from '../../Review/ReviewPatch'
 import Star from '../../Review/Star'
@@ -10,56 +10,52 @@ const ProfileCard = props => {
   const { list, msgAlert, user } = props
   const jsx = (
     <Fragment>{list.map(p => (
-      <Container key={p._id}>
-        <Row>
-          <Col xl={4} lg={6} md={8} sm={10} xs={12} className="mx-auto">
-            <br/>
-            <Card className={`d-flex border-${p.interest === 'Lessons'
+      <div key={p._id}>
+        <br/>
+        <Card className={`d-flex border-${p.interest === 'Lessons'
+          ? 'primary' : p.interest === 'Jams' ? 'success' : p.interest === 'Gigs' ? 'danger' : 'white'}`}>
+          <Card.Body>
+            <Card.Title className={`align-items-center d-flex justify-content-center text-${p.interest === 'Lessons'
               ? 'primary' : p.interest === 'Jams' ? 'success' : p.interest === 'Gigs' ? 'danger' : 'white'}`}>
-              <Card.Body>
-                <Card.Title className={`align-items-center d-flex justify-content-center text-${p.interest === 'Lessons'
-                  ? 'primary' : p.interest === 'Jams' ? 'success' : p.interest === 'Gigs' ? 'danger' : 'white'}`}>
-                  {p.instrument} {p.interest}
-                </Card.Title>
-                <Card.Subtitle className="align-items-center d-flex justify-content-center mb-2 text-muted">{p.city}, {p.state}</Card.Subtitle>
-                <Card.Subtitle className="align-items-center d-flex justify-content-center mb-2 text-info">{p.owner.name}</Card.Subtitle>
-                <Card.Text className="align-items-center d-flex justify-content-center mb-2 text-secondary">{p.blurb}</Card.Text>
-              </Card.Body>
-              <Accordion>
-                <Card className="d-flex border-0">
-                  <Card.Header className="myaccordion align-items-center d-flex justify-content-center border-0">
-                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                      { p.reviews.length > 1
-                        ? <div><Star reviews={p.reviews}/><span className="small-text"> {p.reviews.length} reviews</span></div>
-                        : p.reviews.length === 1
-                          ? <div><Star reviews={p.reviews}/><span className="small-text"> {p.reviews.length} review</span></div>
-                          : <span className="small-text">No reviews</span>}
-                    </Accordion.Toggle>
-                  </Card.Header>
-                  {p.reviews.map(review => (
-                    <Accordion.Collapse key={review._id} eventKey="0">
-                      <Card className="d-flex">
-                        <Card.Body>
-                          <span className="align-items-center d-flex justify-content-center"><Star rating={review.rating}/></span>
-                          <Card.Text className="align-items-center d-flex justify-content-center small-text">{review.content}</Card.Text>
-                          <Card.Text className="align-items-center d-flex justify-content-center italic text-muted small-text">{review.reviewerName}</Card.Text>
-                          <ReviewPatch {...props} review={review} profileId={p._id} user={user} msgAlert={msgAlert}/>
-                        </Card.Body>
-                      </Card>
-                    </Accordion.Collapse>
-                  ))}
-                  <Card.Footer className="myaccordion align-items-center d-flex justify-content-center border-0">
-                    {user && user._id === p.owner._id
-                      ? <Link to={`/profiles/${p._id}`}><Button variant="outline-success" className="mr-auto"><FaUserEdit/> Edit Profile</Button></Link>
-                      : <ReviewPost {...props} profile={p} user={user} msgAlert={msgAlert}/>}
-                  </Card.Footer>
-                </Card>
-              </Accordion>
+              {p.instrument} {p.interest}
+            </Card.Title>
+            <Card.Subtitle className="align-items-center d-flex justify-content-center mb-2 text-muted">{p.city}, {p.state}</Card.Subtitle>
+            <Card.Subtitle className="align-items-center d-flex justify-content-center mb-2 text-info">{p.owner.name}</Card.Subtitle>
+            <Card.Text className="align-items-center d-flex justify-content-center mb-2 text-secondary">{p.blurb}</Card.Text>
+          </Card.Body>
+          <Accordion>
+            <Card className="d-flex border-0">
+              <Card.Header className="myaccordion align-items-center d-flex justify-content-center border-0">
+                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                  { p.reviews.length > 1
+                    ? <div><Star reviews={p.reviews}/><span className="small-text"> {p.reviews.length} reviews</span></div>
+                    : p.reviews.length === 1
+                      ? <div><Star reviews={p.reviews}/><span className="small-text"> {p.reviews.length} review</span></div>
+                      : <span className="small-text">No reviews</span>}
+                </Accordion.Toggle>
+              </Card.Header>
+              {p.reviews.map(review => (
+                <Accordion.Collapse key={review._id} eventKey="0">
+                  <Card className="d-flex">
+                    <Card.Body>
+                      <span className="align-items-center d-flex justify-content-center"><Star rating={review.rating}/></span>
+                      <Card.Text className="align-items-center d-flex justify-content-center small-text">{review.content}</Card.Text>
+                      <Card.Text className="align-items-center d-flex justify-content-center italic text-muted small-text">{review.reviewerName}</Card.Text>
+                      <ReviewPatch {...props} review={review} profileId={p._id} user={user} msgAlert={msgAlert}/>
+                    </Card.Body>
+                  </Card>
+                </Accordion.Collapse>
+              ))}
+              <Card.Footer className="myaccordion align-items-center d-flex justify-content-center border-0">
+                {user && user._id === p.owner._id
+                  ? <Link to={`/profiles/${p._id}`}><Button variant="outline-success" className="mr-auto"><FaUserEdit/> Edit Profile</Button></Link>
+                  : <ReviewPost {...props} profile={p} user={user} msgAlert={msgAlert}/>}
+              </Card.Footer>
             </Card>
-            <br/>
-          </Col>
-        </Row>
-      </Container>
+          </Accordion>
+        </Card>
+        <br/>
+      </div>
     ))}
     </Fragment>
   )
