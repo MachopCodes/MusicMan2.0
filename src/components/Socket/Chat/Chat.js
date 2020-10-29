00143dd54285d3ab2d8dcb57c7d78765f577e04b
MachopCodes <william.andreae@uconn.edu>
2020-10-29 16:10:12 -0400

    added heroku url to sockets

--- src/components/Socket/Chat/Chat.js
+++ src/components/Socket/Chat/Chat.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react'
+import { msgFrom, msgTo } from '../../../api/message'
+import apiUrl from '../../../api/config'
 import queryString from 'query-string'
 import io from 'socket.io-client'
-// import TextContainer from '../TextContainer/TextContainer'
 import Messages from '../Messages/Messages'
 import InfoBar from '../InfoBar/InfoBar'
 import Input from '../Input/Input'
-import { msgFrom, msgTo } from '../../../api/message'
 import './Chat.css'
-const ENDPOINT = 'http://localhost:4741'
+const ENDPOINT = apiUrl
 let socket
 
 const Chat = ({ user, location, setUser, opers, setOpers }) => {
