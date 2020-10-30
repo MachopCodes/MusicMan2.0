5610682c33f6080dabbbc1c63e9726da68b62d00
MachopCodes <william.andreae@uconn.edu>
2020-10-30 10:52:30 -0400

    added console.logs

--- src/components/Socket/Chat/Chat.js
+++ src/components/Socket/Chat/Chat.js
@@ -3,30 +3,41 @@ import { msgFrom, msgTo } from '../../../api/message'
 import apiUrl from '../../../api/config'
 import queryString from 'query-string'
 import io from 'socket.io-client'
+
 import Messages from '../Messages/Messages'
 import InfoBar from '../InfoBar/InfoBar'
 import Input from '../Input/Input'
+
 import './Chat.css'
+
 const ENDPOINT = apiUrl
 let socket
 
 const Chat = ({ user, location, setUser, opers, setOpers }) => {
   if (user) {
     const { name, room, to } = queryString.parse(location.search)
-    const filteredMessages = []; user.messages.map(m => {
+    const filteredMessages = []
+    user.messages.map(m => {
       if (m.receiverId === to || m.senderId === to) { filteredMessages.push(m) }
-    }); const [msgs, setMsgs] = useState(filteredMessages); const [msg, setMsg] = useState('')
+    })
+    const [msgs, setMsgs] = useState(filteredMessages)
+    const [msg, setMsg] = useState('')
     useEffect(() => {
-      socket = io(ENDPOINT); socket.emit('join', { name, room }, (error) => {
+      socket = io(ENDPOINT)
+      socket.emit('join', { name, room }, (error) => {
+        console.log('joining room')
         if (error) { alert(error) }
-      }); return () => socket.close()
+      })
+      return () => socket.close()
     }, [ENDPOINT, location.search])
     useEffect(() => {
       socket.on('message', msg => {
+        console.log('sending message')
         setMsgs(msgs => [ ...msgs, msg ])
         msgTo(msg, user, to, room); msgFrom(msg, user, to, room).then((res) => setUser(res.data))
       }); socket.on('roomData', ({ opers }) => setOpers(opers))
-    }, []); const sendMessage = (e) => {
+    }, [])
+    const sendMessage = (e) => {
       e.preventDefault(); if (msg) { socket.emit('sendMessage', msg, () => setMsg('')) }
     }; return (
       <div className='outerContainer'>
