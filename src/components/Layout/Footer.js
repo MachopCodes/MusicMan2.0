5542c30fe1bf00ca57ef66bc4e5c918f573b6148
MachopCodes <william.andreae@uconn.edu>
2020-12-15 17:23:09 -0500

    fixed footer name logic

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -3,7 +3,12 @@ import { Navbar, Nav } from 'react-bootstrap'
 import logo from '../../icons/wa-logo.png'
 
 const Footer = ({ user }) => {
-  let name; if (user) name = user.name.substr(0, user.name.indexOf(' '))
+  let name
+  if (user) {
+    user.name.includes(' ')
+      ? name = user.name.substr(0, user.name.indexOf(' '))
+      : name = user.name
+  }
   return (
     <Navbar className="d-flex" bg="light" fixed="bottom" style={{ color: 'grey', textAlign: 'right' }}>
       <Nav className="mr-auto">{ user && <span className="navbar-text mr-2">Hi, {name}!</span>}</Nav>
